8d1c54b6692181ee8bca69210f04fa4716836736
priyashukla1411 <priyashukla2k@gmail.com>
2022-06-29 17:58:12 +0530

    first commit

---
+++ question2.py
@@ -0,0 +1,85 @@
+#                          >>>>>>>>>  3
+# import datetime
+# x = datetime.datetime.now()
+# print ("Current date and time : ")
+# print (x.strftime("%Y-%m-%d %H:%M:%S"))  # strftime(formating)
+
+
+#                   >>>>>>>>>>>>    4
+# a = float(input ("enter the the radius =  "))
+# p = 3.14*(a*a)
+# print("area of circle = ", p)
+
+#                     >>>>>>>>>>>  6
+# number = input("user input numbers : ")
+# list = number.split(",")
+# tuple = tuple(list)
+# print('List : ',list)
+# print('Tuple : ',tuple)
+
+#                   >>>>>>>>>>>  7
+# filename= input("Enter Filename: ")
+# f = filename.split(".")
+# print ("Extension of the file is : ", f[-1])
+ 
+ 
+ 
+#                  >>>>>>>>>>>  8
+# colour = input ("Enetr colors name= ")
+# f = colour.split(",")
+# c= (f[0], f[-1])
+# print(c)
+ 
+
+#                        >>>>>>>>>>>  9
+# schedule = input("enter student schedule : ")
+# f = "/".join(schedule)
+# print(schedule)
+# print(f)
+ 
+ 
+ #                          >>>>>>>>>>> 10
+# a = int(input("Input an integer : "))
+# n1 = int( "%d" % a )
+# n2 = int( "%d%d" % (a,a) )
+# n3 = int( "%d%d%d" % (a,a,a) )
+# print (n1+n2+n3)
+
+#                      >>>>>>>>>>> 12
+# import calendar 
+# y = int(input ("enter year = "))
+# m = int(input("enter month = "))
+# print(calendar.month(y,m))
+
+
+#               >>>>>>>>>>>>>>>>14
+# from datetime import datetime
+# first_year= str(input('Enter date(yyyy-mm-dd): '))
+# my_date1 = datetime.strptime(first_year, "%Y-%m-%d")
+# print(my_date1)
+# last_year = str(input('Enter date(yyyy-mm-dd): '))
+# my_date2 = datetime.strptime(last_year, "%Y-%m-%d")
+# print(my_date2)
+
+# delta = my_date2 - my_date1
+# print(delta.days)
+
+#              >>>>>>>>>>>>>>15
+
+# num= int(input("enter the radius = "))
+# V=4/3*(3.14*num*3)
+# print(V)
+
+#             >>>>>>>>>>>>>>>16
+# def difference(n):
+#     if n <= 17:
+#         return 17 - n
+#     else:
+#         return (n - 17) * 2 
+
+# print(difference(18))
+
+
+
+
+
